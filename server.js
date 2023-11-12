c6901d43a93534b2a1912f3de9e96231b3babdff
MiguelAcevedo <miguela@gigflow.co>
2023-11-11 19:32:19 -0500

    port changed

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.use(cors());
 
 app.use('/api', jsonServerApp);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
